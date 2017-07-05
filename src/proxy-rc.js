25e7cab45fdd7e7a470426df309c53c5a78bacde
ridel1e <ridellee@gmail.com>
2017-07-05 20:41:06 +0300

    initial commit

---
+++ src/proxy-rc.js
@@ -0,0 +1,20 @@
+
+/*
+ * Default config object
+ */
+const defaultConf = {
+
+};
+
+/*
+ * HTTP method constants. 
+ */
+const methods = {
+  
+}
+
+const createRC = (conf) => {
+  // const currentConf = Object.assign({})
+};
+
+export { createRC }
